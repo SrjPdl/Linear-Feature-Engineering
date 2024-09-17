de35661bce34e5037d2f1b1dfde298b5b2ec414f
SrjPdl <asdfsurajpoudel@gmail.com>
2024-09-17 01:07:53 -0400

    Fix: Minor fixes

--- main.py
+++ main.py
@@ -21,20 +21,19 @@ def main():
     new_model = Model(X_new, y, config["k_folds"])
     PLOT_STATE = "selected_5"
 
-    print("Train MSE with all features: ", sum((y - predictions)**2)/len(y))
     print("Best model degree with all 8 features:", best_degree, "\n")
 
-    predictions, best_degree, best_model = new_model.train(degrees, PLOT_STATE)
+    _, best_degree, best_model = new_model.train(degrees, PLOT_STATE)
     X_test_selected = test_data.values[:,best_feature_indices]
     test_predictions = new_model.get_predictions(X_test_selected, (best_degree, best_model))
     write_predictions_to_file(test_predictions, config["prediction_file_path"])
 
+    print(f"Best model degree with only {best_feature_indices} features:", best_degree)
+
     PLOT_STATE = "selected_4"
     new_4_model = Model(X_new[:,:-2], y, config["k_folds"])
-    predictions, best_degree, best_model = new_4_model.train(degrees, PLOT_STATE)
+    _, best_degree, best_model = new_4_model.train(degrees, PLOT_STATE)
     
-    print(f"Train MSE with only {best_feature_indices} features:", sum((y - predictions)**2)/len(y))
-    print(f"Best model degree with only {best_feature_indices} features:", best_degree)
 
 if __name__ == '__main__':
     main()
